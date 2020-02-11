13e977b56e233db4d976dcd9996bacf75eb9daf6
Abelsp04 <Abelsegui@hotmail.com>
2020-02-11 08:29:31 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,48 @@
 import "../style/index.scss";
 
-window.onload =()=> {
-  document.querySelector('card').classList.add(generateRandomSuit());
-   document.querySelector('card').innerHTML = generateRandomNumber();
-  
+window.onload = () => {
+  document.querySelector("card").classList.add(generateRandomSuit());
+  document.querySelector("card").innerHTML = generateRandomNumber();
 };
 
-let generateRandomNumber=()=>{
-   
-    let numbers =["A","2","3","4","5","6","7","8","9","10","j","Q","K"];
-    let indexNumbers =Math.floor(Math.random() * numbers.length);
-    return numbers[indexNumbers];
-}
-let generateRandomNumber=()=>{
-   
-    let numbers =["A","2","3","4","5","6","7","8","9","10","j","Q","K"];
-    let suit = ["diamond", "spade", "heart","club"];
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "j",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "j",
+    "Q",
+    "K"
+  ];
+  let suit = ["diamond", "spade", "heart", "club"];
 
-    let indexNumbers =Math.floor(Math.random() * numbers.length);
-    let indexNumbers =Math.floor(Math.random() * suit.length);
-    return suit[indexSuit];
-}
-
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  let indexNumbers = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+};
